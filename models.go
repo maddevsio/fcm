966dad05e524930f4c7c0d9b8933e935e7230e5a
Gen1us2k <minkin.andrew@gmail.com>
2019-03-08 17:21:11 +0600

    Added support of channel_id. Fixes #9

--- models.go
+++ models.go
@@ -160,6 +160,12 @@ type (
 		// Body indicates notification body text.
 		Body string `json:"body,omitempty"`
 
+		// AndroidChannelID The notification's channel id (new in Android O).
+		// The app must create a channel with this channel ID before any notification with this channel ID is received.
+		// If you don't send this channel ID in the request, or if the channel ID provided has
+		// not yet been created by the app, FCM uses the channel ID specified in the app manifest.
+		AndroidChannelID string `json:"android_channel_id,omitempty"`
+
 		// Sound indicates a sound to play when the device receives a notification.
 		// Sound files can be in the main bundle of the client app or in the
 		// Library/Sounds folder of the app's data container.
